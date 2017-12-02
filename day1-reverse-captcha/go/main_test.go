6ca55e78d4b89683b54ab849237b619022fe6ae6
Reiner Gerecke <me@reinergerecke.de>
2017-12-02 18:11:41 +0100

    Day 1: Go - step two

--- day1-reverse-captcha/go/main_test.go
+++ day1-reverse-captcha/go/main_test.go
@@ -12,3 +12,11 @@ func TestReverseCaptcha(t *testing.T) {
 	assert.Equal(t, reverseCaptcha("1234"), 0)
 	assert.Equal(t, reverseCaptcha("91212129"), 9)
 }
+
+func TestReverseCaptchaStepTwo(t *testing.T) {
+	assert.Equal(t, reverseCaptchaStepTwo("1212"), 6)
+	assert.Equal(t, reverseCaptchaStepTwo("1221"), 0)
+	assert.Equal(t, reverseCaptchaStepTwo("123425"), 4)
+	assert.Equal(t, reverseCaptchaStepTwo("123123"), 12)
+	assert.Equal(t, reverseCaptchaStepTwo("12131415"), 4)
+}
