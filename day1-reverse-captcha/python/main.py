b97ad33845c58be5f92457bcb92999b7c2b900ec
Reiner Gerecke <me@reinergerecke.de>
2017-12-02 20:37:27 +0100

    Day 1: Python - step two

--- day1-reverse-captcha/python/main.py
+++ day1-reverse-captcha/python/main.py
@@ -15,6 +15,17 @@ def reverse_captcha(value):
     )
 
 
+def reverse_captcha2(value):
+    value = [int(c) for c in value]
+    lookahead = len(value) // 2
+
+    return sum(
+        a
+        for idx, a in enumerate(value)
+        if a == value[(idx + lookahead) % len(value)]
+    )
+
+
 class TestCase(unittest.TestCase):
 
     def test_reverse(self):
@@ -23,6 +34,13 @@ class TestCase(unittest.TestCase):
         self.assertEqual(reverse_captcha('1234'), 0)
         self.assertEqual(reverse_captcha('91212129'), 9)
 
+    def test_reverse2(self):
+        self.assertEqual(reverse_captcha2('1212'), 6)
+        self.assertEqual(reverse_captcha2('1221'), 0)
+        self.assertEqual(reverse_captcha2('123425'), 4)
+        self.assertEqual(reverse_captcha2('123123'), 12)
+        self.assertEqual(reverse_captcha2('12131415'), 4)
+
 
 if __name__ == '__main__':
-    print(reverse_captcha(sys.argv[1]))
+    print(reverse_captcha2(sys.argv[1]))
